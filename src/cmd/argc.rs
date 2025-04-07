bba2c2ee4683fa2fb527ca828e6faeacc3dead06
χ <voidbyte@null.net>
2025-04-07 19:35:27 +0200

    . add kubernetes ctx creation feature

--- src/cmd/argc.rs
+++ src/cmd/argc.rs
@@ -1,70 +1,71 @@
 use clap::{crate_version, Arg, Command};
 
 pub fn cmd_app() -> Command<'static> {
-	Command::new("kdd")
-		.version(&crate_version!()[..])
-		.about("Kubernetes Driven Development and Deployment")
-		.arg(arg_root_dir())
-		.subcommand(sub_build())
-		.subcommand(sub_watch())
-		.subcommand(sub_dbuild())
-		.subcommand(sub_dpush())
-		.subcommand(sub_realm())
-		.subcommand(sub_ktemplate())
-		.subcommand(sub_kaction(("kapply", "ka")))
-		.subcommand(sub_kaction(("kcreate", "kc")))
-		.subcommand(sub_kaction(("kdelete", "kd")))
-		.subcommand(sub_klog())
-		.subcommand(sub_kexec())
-		.subcommand(sub_version())
+    Command::new("kdd")
+        .version(&crate_version!()[..])
+        .about("Kubernetes Driven Development and Deployment")
+        .arg(arg_root_dir())
+        .subcommand(sub_build())
+        .subcommand(sub_watch())
+        .subcommand(sub_dbuild())
+        .subcommand(sub_dpush())
+        .subcommand(sub_realm())
+        .subcommand(sub_ktemplate())
+        .subcommand(sub_kaction(("kapply", "ka")))
+        .subcommand(sub_kaction(("kcreate", "kc")))
+        .subcommand(sub_kaction(("kdelete", "kd")))
+        .subcommand(sub_klog())
+        .subcommand(sub_kexec())
+        .subcommand(sub_context_create())
+        .subcommand(sub_version())
 }
 
 // region:    Subcommands
 fn sub_build() -> Command<'static> {
-	Command::new("build")
-		.about("Build one or more block")
-		.arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
-		.arg(arg_root_dir())
+    Command::new("build")
+        .about("Build one or more block")
+        .arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
+        .arg(arg_root_dir())
 }
 
 fn sub_watch() -> Command<'static> {
-	Command::new("watch")
-		.about("Watch one or more block")
-		.arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
-		.arg(arg_root_dir())
+    Command::new("watch")
+        .about("Watch one or more block")
+        .arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
+        .arg(arg_root_dir())
 }
 
 fn sub_dbuild() -> Command<'static> {
-	Command::new("dbuild")
-		.about("Build and docker build one or more block")
-		.arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
-		.arg(arg_root_dir())
+    Command::new("dbuild")
+        .about("Build and docker build one or more block")
+        .arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
+        .arg(arg_root_dir())
 }
 
 fn sub_dpush() -> Command<'static> {
-	Command::new("dpush")
-		.about("Docker push one or more block to a realm")
-		.arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
-		.arg(arg_root_dir())
+    Command::new("dpush")
+        .about("Docker push one or more block to a realm")
+        .arg(Arg::new("blocks").help("Comma delimited block names (no space)"))
+        .arg(arg_root_dir())
 }
 
 fn sub_realm() -> Command<'static> {
-	Command::new("realm")
-		.about("Show the available realms or set the current realm")
-		.arg(Arg::new("name").help("Realm name to change to"))
-		.arg(arg_root_dir())
+    Command::new("realm")
+        .about("Show the available realms or set the current realm")
+        .arg(Arg::new("name").help("Realm name to change to"))
+        .arg(arg_root_dir())
 }
 
 fn sub_kaction(action_n_alias: (&'static str, &'static str)) -> Command<'static> {
-	let (action, alias) = action_n_alias;
-	// with format!(...)
-	let about = match action {
-		"kapply" => "Excute kubectl apply for one or more kubernetes configuration file",
-		"kcreate" => "Excute kubectl create for one or more kubernetes configuration file",
-		"kdelete" => "Excute kubectl delete for one or more kubernetes configuration file",
-		_ => "not supported",
-	};
-	Command::new(action)
+    let (action, alias) = action_n_alias;
+    // with format!(...)
+    let about = match action {
+        "kapply" => "Excute kubectl apply for one or more kubernetes configuration file",
+        "kcreate" => "Excute kubectl create for one or more kubernetes configuration file",
+        "kdelete" => "Excute kubectl delete for one or more kubernetes configuration file",
+        _ => "not supported",
+    };
+    Command::new(action)
 		.about(about)
 		.alias(alias)
 		.arg(
@@ -75,7 +76,7 @@ fn sub_kaction(action_n_alias: (&'static str, &'static str)) -> Command<'static>
 }
 
 fn sub_ktemplate() -> Command<'static> {
-	Command::new("ktemplate")
+    Command::new("ktemplate")
 		.about("Render the k8s yaml files for the current realm")
 		.arg(
 			Arg::new("names")
@@ -84,8 +85,15 @@ fn sub_ktemplate() -> Command<'static> {
 		.arg(arg_root_dir())
 }
 
+fn sub_context_create() -> Command<'static> {
+    Command::new("kctxcreate")
+        .about("Create a new kubernetes context")
+        .arg(Arg::new("name").help("Name of the kuberentes context"))
+        .arg(arg_root_dir())
+}
+
 fn sub_klog() -> Command<'static> {
-	Command::new("klog")
+    Command::new("klog")
 		.alias("klogs")
 		.alias("kl")
 		.about("Execute and display the kubectl log for one or more service (and all of their respective pods)")
@@ -97,7 +105,7 @@ fn sub_klog() -> Command<'static> {
 }
 
 fn sub_kexec() -> Command<'static> {
-	Command::new("kexec")
+    Command::new("kexec")
 		.about("Execute a kubectl exec for all pods matching service_name")
 		.alias("kx")
 		.arg(arg_root_dir())
@@ -116,19 +124,19 @@ fn sub_kexec() -> Command<'static> {
 }
 
 fn sub_version() -> Command<'static> {
-	Command::new("version")
-		.about("Version files with the configured version replace commands")
-		.arg(arg_root_dir())
+    Command::new("version")
+        .about("Version files with the configured version replace commands")
+        .arg(arg_root_dir())
 }
 
 // endregion: Subcommands
 
 // region:    Common Args
 fn arg_root_dir() -> Arg<'static> {
-	Arg::new("root_dir")
-		.short('d')
-		.takes_value(true)
-		.help("The root dir where the driving kdd.yaml reside")
+    Arg::new("root_dir")
+        .short('d')
+        .takes_value(true)
+        .help("The root dir where the driving kdd.yaml reside")
 }
 
 // endregion: Common Args
--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -4,8 +4,8 @@
 
 use self::argc::cmd_app;
 use crate::{
-	app_error::AppError,
-	kdd::{error::KddError, Kdd},
+    app_error::AppError,
+    kdd::{error::KddError, Kdd},
 };
 use clap::ArgMatches;
 use std::path::Path;
@@ -13,174 +13,201 @@ use std::path::Path;
 mod argc;
 
 pub fn cmd_run() -> Result<(), AppError> {
-	let app = cmd_app().get_matches();
-	let root_dir = app
-		.value_of("root_dir")
-		.or_else(|| {
-			if let Some((_, sub)) = &app.subcommand() {
-				sub.value_of("root_dir")
-			} else {
-				None
-			}
-		})
-		.unwrap_or("./");
+    let app = cmd_app().get_matches();
+    let root_dir = app
+        .value_of("root_dir")
+        .or_else(|| {
+            if let Some((_, sub)) = &app.subcommand() {
+                sub.value_of("root_dir")
+            } else {
+                None
+            }
+        })
+        .unwrap_or("./");
 
-	match app.subcommand() {
-		Some(("build", sub_cmd)) => exec_build(root_dir, sub_cmd, false)?,
-		Some(("watch", sub_cmd)) => exec_watch(root_dir, sub_cmd)?,
-		Some(("dbuild", sub_cmd)) => exec_build(root_dir, sub_cmd, true)?,
-		Some(("dpush", sub_cmd)) => exec_dpush(root_dir, sub_cmd)?,
-		Some(("realm", sub_cmd)) => exec_realm(root_dir, sub_cmd)?,
-		Some(("ktemplate", sub_cmd)) => exec_kaction("template", root_dir, sub_cmd)?,
-		Some(("kapply", sub_cmd)) => exec_kaction("apply", root_dir, sub_cmd)?,
-		Some(("kcreate", sub_cmd)) => exec_kaction("create", root_dir, sub_cmd)?,
-		Some(("kdelete", sub_cmd)) => exec_kaction("delete", root_dir, sub_cmd)?,
-		Some(("klog", sub_cmd)) => exec_klog(root_dir, sub_cmd)?,
-		Some(("kexec", sub_cmd)) => exec_kexec(root_dir, sub_cmd)?,
-		Some(("version", sub_cmd)) => exec_version(root_dir, sub_cmd)?,
-		_ => {
-			// needs cmd_app version as the orginal got consumed by get_matches
-			cmd_app().print_long_help()?;
-			println!("\n");
-		}
-	}
+    match app.subcommand() {
+        Some(("build", sub_cmd)) => exec_build(root_dir, sub_cmd, false)?,
+        Some(("watch", sub_cmd)) => exec_watch(root_dir, sub_cmd)?,
+        Some(("dbuild", sub_cmd)) => exec_build(root_dir, sub_cmd, true)?,
+        Some(("dpush", sub_cmd)) => exec_dpush(root_dir, sub_cmd)?,
+        Some(("realm", sub_cmd)) => exec_realm(root_dir, sub_cmd)?,
+        Some(("ktemplate", sub_cmd)) => exec_kaction("template", root_dir, sub_cmd)?,
+        Some(("kapply", sub_cmd)) => exec_kaction("apply", root_dir, sub_cmd)?,
+        Some(("kcreate", sub_cmd)) => exec_kaction("create", root_dir, sub_cmd)?,
+        Some(("kdelete", sub_cmd)) => exec_kaction("delete", root_dir, sub_cmd)?,
+        Some(("kctxcreate", sub_cmd)) => exec_kctxcreate(root_dir, sub_cmd)?,
+        Some(("klog", sub_cmd)) => exec_klog(root_dir, sub_cmd)?,
+        Some(("kexec", sub_cmd)) => exec_kexec(root_dir, sub_cmd)?,
+        Some(("version", sub_cmd)) => exec_version(root_dir, sub_cmd)?,
+        _ => {
+            // needs cmd_app version as the orginal got consumed by get_matches
+            cmd_app().print_long_help()?;
+            println!("\n");
+        }
+    }
 
-	Ok(())
+    Ok(())
 }
 
 // region:    Command Execs
 fn exec_build(root_dir: &str, argc: &ArgMatches, docker_build: bool) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
+    let kdd = load_kdd(root_dir)?;
 
-	let blocks = argc.value_of("blocks").map(|v| v.split(",").into_iter().collect::<Vec<&str>>());
-	let blocks = blocks.as_ref().map(|v| &v[..]);
+    let blocks = argc
+        .value_of("blocks")
+        .map(|v| v.split(",").into_iter().collect::<Vec<&str>>());
+    let blocks = blocks.as_ref().map(|v| &v[..]);
 
-	kdd.build(blocks, docker_build)?;
+    kdd.build(blocks, docker_build)?;
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_watch(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
-	let blocks = argc.value_of("blocks").map(|v| v.split(",").into_iter().collect::<Vec<&str>>());
-	let blocks = blocks.as_ref().map(|v| &v[..]);
+    let kdd = load_kdd(root_dir)?;
+    let blocks = argc
+        .value_of("blocks")
+        .map(|v| v.split(",").into_iter().collect::<Vec<&str>>());
+    let blocks = blocks.as_ref().map(|v| &v[..]);
 
-	kdd.watch(blocks)?;
+    kdd.watch(blocks)?;
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_dpush(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
+    let kdd = load_kdd(root_dir)?;
 
-	let blocks = split_names(argc.value_of("blocks"));
-	let blocks = blocks.as_ref().map(|v| &v[..]);
+    let blocks = split_names(argc.value_of("blocks"));
+    let blocks = blocks.as_ref().map(|v| &v[..]);
 
-	let realm = kdd.current_realm()?.ok_or_else(|| KddError::DpushFailNoRealm)?;
+    let realm = kdd
+        .current_realm()?
+        .ok_or_else(|| KddError::DpushFailNoRealm)?;
 
-	kdd.d_push(realm, blocks)?;
+    kdd.d_push(realm, blocks)?;
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_realm(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
+    let kdd = load_kdd(root_dir)?;
 
-	if let Some(name) = argc.value_of("name") {
-		println!("Change realm to {}", name);
-		kdd.realm_set(name)?;
-		kdd.print_realms()?;
-	}
-	// if no name, we list the realms
-	else {
-		kdd.print_realms()?;
-	}
+    if let Some(name) = argc.value_of("name") {
+        println!("Change realm to {}", name);
+        kdd.realm_set(name)?;
+        kdd.print_realms()?;
+    }
+    // if no name, we list the realms
+    else {
+        kdd.print_realms()?;
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_kaction(action: &str, root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
-	let realm = kdd.current_realm()?;
-	let names = split_names(argc.value_of("names"));
-	let names = names.as_ref().map(|v| &v[..]);
+    let kdd = load_kdd(root_dir)?;
+    let realm = kdd.current_realm()?;
+    let names = split_names(argc.value_of("names"));
+    let names = names.as_ref().map(|v| &v[..]);
 
-	if let Some(realm) = realm {
-		// if no names, get the names from the realm default_configurations if present
-		let config_names = realm.default_configurations();
-		let names = names.or_else(|| config_names.as_ref().map(|v| v.as_slice()));
+    if let Some(realm) = realm {
+        // if no names, get the names from the realm default_configurations if present
+        let config_names = realm.default_configurations();
+        let names = names.or_else(|| config_names.as_ref().map(|v| v.as_slice()));
 
-		match action {
-			"apply" => kdd.k_apply(realm, names)?,
-			"create" => kdd.k_create(realm, names)?,
-			"delete" => kdd.k_delete(realm, names)?,
-			"template" => kdd.k_templates(realm, names, true).map(|result| ())?,
-			_ => (),
-		}
-	} else {
-		println!("Cannot run '{}', no current realm", action);
-	}
+        match action {
+            "apply" => kdd.k_apply(realm, names)?,
+            "create" => kdd.k_create(realm, names)?,
+            "delete" => kdd.k_delete(realm, names)?,
+            "template" => kdd.k_templates(realm, names, true).map(|result| ())?,
+            _ => (),
+        }
+    } else {
+        println!("Cannot run '{}', no current realm", action);
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_klog(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
-	let realm = kdd.current_realm()?;
+    let kdd = load_kdd(root_dir)?;
+    let realm = kdd.current_realm()?;
 
-	let names: Option<Vec<&str>> = split_names(argc.value_of("names"));
-	let names = names.map(|v| v.into_iter().map(|s| s.to_owned()).collect());
+    let names: Option<Vec<&str>> = split_names(argc.value_of("names"));
+    let names = names.map(|v| v.into_iter().map(|s| s.to_owned()).collect());
 
-	if let Some(realm) = realm {
-		kdd.k_log(realm, names)?;
-	} else {
-		println!("Cannot run kubectl log, no current realm");
-	}
+    if let Some(realm) = realm {
+        kdd.k_log(realm, names)?;
+    } else {
+        println!("Cannot run kubectl log, no current realm");
+    }
 
-	Ok(())
+    Ok(())
+}
+
+fn exec_kctxcreate(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
+    let kdd = load_kdd(root_dir)?;
+    let realm = kdd.current_realm()?;
+
+    if let Some(name) = argc.value_of("name") {
+        println!("Creating {} kubernetes context", name);
+        kdd.k_create_context(name);
+    } else {
+        println!("Cannot run kubectl config set-context , no ctx name. Use `kdd kctxcreate minikube` for example");
+    }
+
+    Ok(())
 }
 
 fn exec_kexec(root_dir: &str, argc: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
-	let realm = kdd.current_realm()?;
+    let kdd = load_kdd(root_dir)?;
+    let realm = kdd.current_realm()?;
 
-	if let Some(realm) = realm {
-		let names = split_names(argc.value_of("names"));
-		let names = names.as_ref().map(|v| &v[..]);
+    if let Some(realm) = realm {
+        let names = split_names(argc.value_of("names"));
+        let names = names.as_ref().map(|v| &v[..]);
 
-		// the pod args (which might contain the cmd for the pod)
-		let pod_args = argc
-			.values_of("pod_args")
-			.map(|v| v.into_iter().map(|d| d.to_string()).collect::<Vec<String>>());
+        // the pod args (which might contain the cmd for the pod)
+        let pod_args = argc.values_of("pod_args").map(|v| {
+            v.into_iter()
+                .map(|d| d.to_string())
+                .collect::<Vec<String>>()
+        });
 
-		if let Some(pod_args) = pod_args {
-			// if -b, then, we add the /bin/bash -c, and the pod_args become one cmd component
-			let pod_args = if argc.is_present("bash") {
-				vec!["/bin/bash".to_string(), "-c".to_string(), pod_args.join(" ")]
-			} else {
-				pod_args
-			};
+        if let Some(pod_args) = pod_args {
+            // if -b, then, we add the /bin/bash -c, and the pod_args become one cmd component
+            let pod_args = if argc.is_present("bash") {
+                vec![
+                    "/bin/bash".to_string(),
+                    "-c".to_string(),
+                    pod_args.join(" "),
+                ]
+            } else {
+                pod_args
+            };
 
-			// make it Vec<&str>
-			let pod_args: Vec<&str> = pod_args.iter().map(|v| v as &str).collect();
-			// call the exec
-			kdd.k_exec(realm, names, &pod_args[..])?;
-		} else {
-			println!("Cannot run kubectl exec , no sub command. Use `kdd kexec -- /bin/bash -c 'ls'` for example");
-		}
-	} else {
-		println!("Cannot run kubectl exec, no current realm");
-	}
+            // make it Vec<&str>
+            let pod_args: Vec<&str> = pod_args.iter().map(|v| v as &str).collect();
+            // call the exec
+            kdd.k_exec(realm, names, &pod_args[..])?;
+        } else {
+            println!("Cannot run kubectl exec , no sub command. Use `kdd kexec -- /bin/bash -c 'ls'` for example");
+        }
+    } else {
+        println!("Cannot run kubectl exec, no current realm");
+    }
 
-	Ok(())
+    Ok(())
 }
 
 fn exec_version(root_dir: &str, _: &ArgMatches) -> Result<(), AppError> {
-	let kdd = load_kdd(root_dir)?;
+    let kdd = load_kdd(root_dir)?;
 
-	kdd.version(&mut std::io::stdout())?;
+    kdd.version(&mut std::io::stdout())?;
 
-	Ok(())
+    Ok(())
 }
 // endregion: Command Execs
 
@@ -188,12 +215,12 @@ fn exec_version(root_dir: &str, _: &ArgMatches) -> Result<(), AppError> {
 
 /// Split a opt str by ","
 fn split_names(val: Option<&str>) -> Option<Vec<&str>> {
-	val.map(|v| v.split(",").into_iter().collect::<Vec<&str>>())
+    val.map(|v| v.split(",").into_iter().collect::<Vec<&str>>())
 }
 
 fn load_kdd(root_dir: &str) -> Result<Kdd, AppError> {
-	let dir = Path::new(root_dir).to_path_buf();
-	Ok(Kdd::from_dir(dir)?)
+    let dir = Path::new(root_dir).to_path_buf();
+    Ok(Kdd::from_dir(dir)?)
 }
 
 // endregion: Utils
